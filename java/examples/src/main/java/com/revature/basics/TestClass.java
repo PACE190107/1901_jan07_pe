33768b3662adb54bb2aa47fd61dc3939d24a1c9d
Yuvi <yuvi@revature.com>
2019-01-10 19:13:30 -0500

    day3 code

---
+++ java/examples/src/main/java/com/revature/basics/TestClass.java
@@ -0,0 +1,27 @@
+package com.revature.basics;
+
+public class TestClass {
+
+	
+}
+//only one public class is allowed
+class A{
+	class C extends B{
+		class D{
+			
+		}
+	}
+}
+
+class B{
+	interface E{
+		
+	}
+}
+
+interface F {
+	interface G extends F{
+		
+	}
+}
+
---
+++ java/examples/src/main/java/com/revature/collection/IteratorExample.java
@@ -0,0 +1,34 @@
+package com.revature.collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class IteratorExample {
+
+	public static void main(String[] args) {
+		ArrayList<Person> Employee = new ArrayList<>();
+		Person mike = new Person("Mike", 100, 19);
+		Person rob = new Person("Rob", 82, 20);
+		Person bob = new Person("Bob", 62, 24);
+		Person kim = new Person("Kim", 73, 21);
+		Person emma = new Person("Emma", 65, 18);
+		
+		Employee.add(mike);
+		Employee.add(rob);
+		Employee.add(bob);
+		Employee.add(kim);
+		Employee.add(emma);
+		
+		Iterator<Person> iter1 = Employee.iterator();
+		System.out.println("======using while======");
+		while(iter1.hasNext()) {
+			Person p = iter1.next();
+			System.out.println(p.name);
+		}
+		System.out.println("======using enhanced for loop======");
+		for(Person p: Employee) {
+			System.out.println(p.name);
+		}
+	}
+
+}
